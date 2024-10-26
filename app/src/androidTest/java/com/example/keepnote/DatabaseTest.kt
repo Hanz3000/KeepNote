91d123e04e690d4d2f53101998bf580082eb8e07
Farhan Abdilah Ramadhan <akunganda787@gmail.com>
2024-10-26 08:28:23 +0700

    menambahkan databasetest farhan

--- app/src/androidTest/java/com/example/keepnote/DatabaseTest.kt
+++ app/src/androidTest/java/com/example/keepnote/DatabaseTest.kt
@@ -20,7 +20,6 @@ class DatabaseTest {
     // Objek contoh untuk tes
     private val category = Category(1, "Belajar")
 
-
     @Before
     fun createDb() {
         val context: Context = ApplicationProvider.getApplicationContext() // Mendapatkan konteks aplikasi
@@ -28,9 +27,8 @@ class DatabaseTest {
         db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java)
             .allowMainThreadQueries()
             .build()
-        //Menginisialisasi objek DAO
+        // Menginisialisasi objek DAO
         categoryDao = db.categoryDao()
-
     }
 
     @After
@@ -39,11 +37,22 @@ class DatabaseTest {
 
     @Test
     @Throws(Exception::class)
-    fun insertAndRetrieveCategory(){
-        categoryDao.insert(category) // Menyisipkan kategori ke dalam tabel
+    fun insertAndRetrieveCategory() {
+        categoryDao.insert(category) // Memasukkan kategori ke dalam database
         val result = categoryDao.getAll() // Mengambil semua kategori dari tabel
-        assert(result.size == 1) // Memastikan bahwa ukuran data yang diterima sama dengan 1
+
+        assert(result.size == 1) // Memastikan bahwa ada satu kategori yang disimpan
+        assert(result[0] == category) // Memastikan kategori yang diambil sesuai dengan kategori yang dimasukkan
     }
 
+    @Test
+    @Throws(Exception::class)
+    fun deleteCategoryByName() {
+        categoryDao.insert(category) // Menyisipkan kategori ke dalam tabel
+        categoryDao.deleteByName(category.name) // Menghapus kategori berdasarkan nama
 
+        val result = categoryDao.getAll() // Mengambil semua kategori dari tabel
+        assert(result.isEmpty()) // Memastikan bahwa tabel kosong setelah penghapusan
+        //farhan boss
+    }
 }
