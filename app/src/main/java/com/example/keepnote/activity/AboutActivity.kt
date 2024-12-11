7ec20a59e1843142bb37c61c9bf89e3008b21839
Farhan Abdilah Ramadhan <akunganda787@gmail.com>
2024-12-11 19:18:31 +0700

    menambahkan tampilan

---
+++ app/src/main/java/com/example/keepnote/activity/AboutActivity.kt
@@ -0,0 +1,12 @@
+package com.example.keepnote.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.keepnote.R
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
