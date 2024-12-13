b65e72ece4e86ddfb03942e816d3fd7043c2b91e
Farhan Abdilah Ramadhan <akunganda787@gmail.com>
2024-12-13 15:00:14 +0700

    Note.kt

--- app/src/main/java/com/example/keepnote/entity/Note.kt
+++ app/src/main/java/com/example/keepnote/entity/Note.kt
@@ -2,7 +2,6 @@ package com.example.keepnote.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.google.firebase.database.Exclude
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -16,7 +15,4 @@ data class Note(
     val category: String, // Kolom untuk kategori catatan
     val timestamp: String = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(Date()) // Kolom untuk timestamp dengan nilai default
 ){
-    // Metode ini digunakan untuk menghindari pengiriman id saat menyimpan ke Firebase
-    @Exclude
-    fun getFirebaseId(): Long? = if (id == 0L) null else id
 }
